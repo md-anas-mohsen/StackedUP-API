34e4fa903e53e260771c12cfdddcc0aef4d4d592
Anas Mohsen <m.mohsen_18634@khi.iba.edu.pk>
2022-10-09 03:40:26 +0500

    added logout

--- services/userService.js
+++ services/userService.js
@@ -239,6 +239,17 @@ const userService = {
       });
     }
   },
+  logout: async (req, res) => {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: MESSAGES.LOGOUT_SUCCESS,
+    });
+  },
 };
 
 module.exports = userService;
